a1a1b0fa1bc21a37a4cb640ba83050c64d2ac802
prafulcool <prafuljcool@gmail.com>
2025-03-09 11:14:56 +0530

    end section 3 video 27

--- API/Program.cs
+++ API/Program.cs
@@ -8,10 +8,19 @@ builder.Services.AddControllers();
 builder.Services.AddDbContext<DataContext>(opt => {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseCors(x => x
+.WithOrigins("http://localhost:4200","https://localhost:4200")
+.AllowAnyHeader()
+.AllowAnyMethod()
+
+//.AllowAnyOrigin() // need to check without this
+);
+
 app.MapControllers();
 
 app.Run();
