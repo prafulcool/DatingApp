f2d6454e22ff287096aa61f916c5e0d81286c499
prafulcool <prafuljcool@gmail.com>
2025-03-27 13:23:20 +0530

    section 5 video 49 min 3:00

---
+++ client/src/app/nav/nav.component.ts
@@ -0,0 +1,28 @@
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AccountService } from '../_services/account.service';
+import { NgIf } from '@angular/common';
+
+@Component({
+  selector: 'app-nav',
+  standalone: true,
+  imports: [FormsModule, NgIf],
+  templateUrl: './nav.component.html',
+  styleUrl: './nav.component.css'
+})
+export class NavComponent {
+  private accountService = inject(AccountService);
+  loggedIn = false;
+model: any = {};
+login(){
+  this.accountService.login(this.model).subscribe({
+    next: response => {
+      console.log(response);
+      this.loggedIn = true;
+    },
+    error: error => console.log(error)
+
+  });
+  console.log(this.model);
+}
+}
